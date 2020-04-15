df027ca16215aef90faf10fbfedf41b7aa906621
Boa <boa.matule@gmail.com>
2020-04-16 00:10:47 +0200

    Configures Animation in Delimiter dots.

--- screens/Welcome.js
+++ screens/Welcome.js
@@ -1,16 +1,19 @@
 import React, {Component }from 'react';
-import { StyleSheet, View, FlatList, Image, Dimensions  } from 'react-native';
+import { StyleSheet, View, FlatList, Image, Dimensions, Animated  } from 'react-native';
 import { Button, Block, Text } from '../components';
 import { theme } from '../constants';
 import { LinearGradient } from 'expo-linear-gradient';
 
-class Welcome extends Component {
+const { width, height } = Dimensions.get('window')
+
+class Welcome extends React.Component {
    static navigationOptions = {
     header : null
   }
+
+  scrollX = new Animated.Value(0);
   
   state = {
-
   }
     renderIllustrations() {
       const { illustrations } = this.props
@@ -33,71 +36,84 @@ class Welcome extends Component {
                 style={{ width, height: height / 2, overflow: 'visible' }}
             />
         )}
+        onScroll={
+          Animated.event([{
+            nativeEvent: { contentOffset: { x: this.scrollX }}
+          }])
+        }
       />
     )
   }
   
-  renderSteps(){
+  renderSteps() {
     const { illustrations } = this.props
-    return(
-      <Block row center middle style={styles.stepsContainer}>
-        {illustrations.map((item, index) => {
-          return (
-            <Block
-              animated
-              flex={false}
-              key={`step-${index}`}
-              color="gray"
-              style={[styles.steps]}
-            />
-          )
-        })}
-      </Block>
-    )
-  }
+    const stepPosition = Animated.divide(this.scrollX, width);
+
+      return(
+        <Block row center middle style={styles.stepsContainer}>
+          {illustrations.map((item, index) => {
+            const opacity = stepPosition.interpolate({
+              inputRange: [index - 1, index, index + 1],
+              outputRange: [0.4, 1, 0.4],
+              extrapolate: 'clamp',
+            });
+
+            return (
+              <Block
+                animated
+                flex={false}
+                key={`step-${index}`}
+                color="gray"
+                style={[styles.steps, { opacity }]}
+              />
+            )
+          })}
+        </Block>
+      )
+    }
   
-  render() {
-    return (
-      <Block>
-        <Block center bottom flex={0.4}>
-          <Text h1 center bold>
-            Your Home.
-            <Text h1 primary> Greener.</Text>
-          </Text>
-          <Text h3 gray2 style={{ marginTop: theme.sizes.padding / 2 }}>
-            Enjoy the experience.
-          </Text>
-        </Block>
-      
-
-        <Block center middle>
-          {this.renderIllustrations()}
-          {this.renderSteps()}
-        </Block>
+    render() {
+      return (
+        <Block>
+          <Block center bottom flex={0.4}>
+            <Text h1 center bold>
+              Your Home.
+              <Text h1 primary> Greener.</Text>
+            </Text>
+            <Text h3 gray2 style={{ marginTop: theme.sizes.padding / 2 }}>
+              Enjoy the experience.
+            </Text>
+          </Block>
         
-        <Block middle flex={0.5} margin={[0, theme.sizes.padding * 2]}>
-          <Button onPress={() => this.props.navigation.navigate('Login')}>
-            <Text center semibold white>Login</Text>
-          </Button>
-          <Button shadow onPress={() => this.props.navigation.navigate('SignUp')}>
-            <Text center semibold>Signup</Text>
-          </Button>
-          <Button onPress={() => {}}>
-            <Text center caption gray>Terms of service</Text>
-          </Button>
-        </Block>
-      </Block>
-    );
-  }
-}
 
-Welcome.defaultProps = {
-  illustrations: [
-    { id: 1, source: require('../assets/images/illustration_1.png') },
-    { id: 2, source: require('../assets/images/illustration_2.png') },
-    { id: 3, source: require('../assets/images/illustration_3.png') },
-  ],
-};
+          <Block center middle>
+            {this.renderIllustrations()}
+            {this.renderSteps()}
+          </Block>
+          
+          <Block middle flex={0.5} margin={[0, theme.sizes.padding * 2]}>
+            <Button onPress={() => this.props.navigation.navigate('Login')}>
+              <Text center semibold white>Login</Text>
+            </Button>
+            <Button shadow onPress={() => this.props.navigation.navigate('SignUp')}>
+              <Text center semibold>Signup</Text>
+            </Button>
+            <Button onPress={() => {}}>
+              <Text center caption gray>Terms of service</Text>
+            </Button>
+          </Block>
+        </Block>
+      );
+    }
+  }
+
+  Welcome.defaultProps = {
+    illustrations: [
+      { id: 1, source: require('../assets/images/illustration_1.png') },
+      { id: 2, source: require('../assets/images/illustration_2.png') },
+      { id: 3, source: require('../assets/images/illustration_3.png') },
+    ],
+  };
 
 export default Welcome;
 
