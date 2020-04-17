b67b3ff09a48d7948845d9891b7dca745962ffa6
Boa <boa.matule@gmail.com>
2020-04-17 18:47:05 +0200

    Adds Header and Input fields(email, username and password); Wraps the Input fields with the KeyboardAvoidingView as the parent component and styles it. WIP

--- screens/Signup.js
+++ screens/Signup.js
@@ -18,12 +18,49 @@ export default class Welcome extends React.Component {
  }
  render(){
    return (
-     <Block middle>
-       <Text>Sign Up</Text>
+    <KeyboardAvoidingView style={styles.signup} behavior="padding">
+       <Block padding={[0, theme.sizes.base * 2]} >
+        <Text h1 bold>Sign Up</Text>
+        <Block middle>
+        <Input 
+          email
+          label="Email"
+          style={[styles.input]}
+          defaultValue={this.state.email}
+          onChangeText={text => this.setState({ email: text })}
+        />
+        <Input
+          label="Username"
+          style={[styles.input]}
+          defaultValue={this.state.username}
+          onChangeText={text => this.setState({ username: text })}
+        />
+        <Input
+          secure
+          label="Password"
+          style={[styles.input]}
+          defaultValue={this.state.password}
+          onChangeText={text => this.setState({ password: text })}
+        />
+        </Block>
      </Block>
+    </KeyboardAvoidingView>
     
    );
  }
  }
 const styles = StyleSheet.create({
+  signup: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  input: {
+    borderRadius: 0,
+    borderWidth: 0,
+    borderBottomColor: theme.colors.gray2,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+  hasErrors: {
+    borderBottomColor: theme.colors.accent,
+  }
 });
