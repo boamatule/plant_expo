64e748520d4b980c042417e7a8ed575c77710bb7
Boa <boa.matule@gmail.com>
2020-04-17 23:44:47 +0200

    Categories Section implemented sucessfully.

--- constants/mocks.js
+++ constants/mocks.js
@@ -1,47 +1,46 @@
-import { theme } from '../constants';
 const categories = [
   {
     id: 'plants',
     name: 'Plants',
     tags: ['products', 'inspirations'],
     count: 147,
-    image: require('../assets/images/plants_1.png')
+    image: require('../assets/icons/plants_1.png')
   },
-  // {
-  //   id: 'seeds',
-  //   name: 'Seeds',
-  //   tags: ['products', 'shop'],
-  //   count: 16,
-  //   image: require('../assets/icons/seeds.png')
-  // },
-  // {
-  //   id: 'flowers',
-  //   name: 'Flowers',
-  //   tags: ['products', 'inspirations'],
-  //   count: 68,
-  //   image: require('../assets/icons/flowers.png')
-  // },
-  // {
-  //   id: 'sprayers',
-  //   name: 'Sprayers',
-  //   tags: ['products', 'shop'],
-  //   count: 17,
-  //   image: require('../assets/icons/sprayers.png')
-  // },
-  // {
-  //   id: 'pots',
-  //   name: 'Pots',
-  //   tags: ['products', 'shop'],
-  //   count: 47,
-  //   image: require('../assets/icons/pots.png')
-  // },
-  // {
-  //   id: 'fertilizers',
-  //   name: 'fertilizers',
-  //   tags: ['products', 'shop'],
-  //   count: 47,
-  //   image: require('../assets/icons/fertilizers.png')
-  // },
+  {
+    id: 'seeds',
+    name: 'Seeds',
+    tags: ['products', 'shop'],
+    count: 16,
+    image: require('../assets/icons/seeds.png')
+  },
+  {
+    id: 'flowers',
+    name: 'Flowers',
+    tags: ['products', 'inspirations'],
+    count: 68,
+    image: require('../assets/icons/flowers.png')
+  },
+  {
+    id: 'sprayers',
+    name: 'Sprayers',
+    tags: ['products', 'shop'],
+    count: 17,
+    image: require('../assets/icons/sprayers.png')
+  },
+  {
+    id: 'pots',
+    name: 'Pots',
+    tags: ['products', 'shop'],
+    count: 47,
+    image: require('../assets/icons/pots.png')
+  },
+  {
+    id: 'fertilizers',
+    name: 'fertilizers',
+    tags: ['products', 'shop'],
+    count: 47,
+    image: require('../assets/icons/fertilizers.png')
+  },
 ];
 
 const products = [
@@ -84,7 +83,6 @@ const profile = {
   monthly_cap: 7000,
   notifications: true,
   newsletter: false,
-  // backgroundColor: theme.colors.grey,
 };
 
 export {
--- screens/Browse.js
+++ screens/Browse.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import { StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { StyleSheet, Image, TouchableOpacity, ScrollView, Dimensions } from 'react-native';
 import { theme, mocks } from '../constants';
-import { Button, Text, Block } from '../components';
+import { Button, Text, Block, Card, Badge } from '../components';
 
 class Browse extends React.Component {  
   state = {
     active: 'Products',
   }
-
 
   renderTab(tab) {
     const { active } = this.state;
@@ -27,9 +26,11 @@ class Browse extends React.Component {
       </TouchableOpacity>
     )
   }
+  
   render() {
-    const { profile } = this.props
+    const { profile, navigation, categories  } = this.props
     const tabs = ['Products', 'Inspirations', 'Shop']
+    
     return (
       <Block> 
         <Block flex={false} row center space="between" style={styles.header}>    
@@ -41,15 +42,38 @@ class Browse extends React.Component {
         <Block flex={false} row style={styles.tabs}>
           {tabs.map(tab => this.renderTab(tab))}
         </Block>
-    </Block>
+          <ScrollView 
+            showsVerticalScrollIndicator={false}
+            style={{paddingVertial: theme.sizes.base * 2}}
+          >
+          <Block flex={false} row space="between" style={styles.categories}>
+            {categories.map(category => (
+              <TouchableOpacity
+                key={category.name}
+                onPress={() => navigation.navigate('Explore', { category })}
+              >
+                <Card center middle shadow style={styles.category}>
+                  <Badge margin={[0, 0, 15]} size={50} color="rgba(41,216,143,0.20)">
+                    <Image source={category.image} />
+                  </Badge>
+                  <Text medium height={20}>{category.name}</Text>
+                  <Text gray caption>{category.count} products</Text>
+                </Card>
+              </TouchableOpacity>
+            ))}
+          </Block>  
+        </ScrollView>
+      </Block>
     );
   }
 }
 
 Browse.defaultProps = {
-  profile: mocks.profile
+  profile: mocks.profile,
+  categories : mocks.categories
 }
 
+const { width } = Dimensions.get('window');
 const styles = StyleSheet.create({
   header: {
     paddingHorizontal: theme.sizes.base * 2,
@@ -73,6 +97,18 @@ const styles = StyleSheet.create({
     borderBottomColor: theme.colors.secondary,
     borderBottomWidth: 3,
   },
+  category: {
+    // this should be dynamic based on screen width
+    minWidth: (width - (theme.sizes.padding * 2.4) - theme.sizes.base) / 2,
+    maxWidth: (width - (theme.sizes.padding * 2.4) - theme.sizes.base) / 2,
+    maxHeight: (width - (theme.sizes.padding * 2.4) - theme.sizes.base) / 2,
+  },
+  categories: {
+    flexWrap: 'wrap',
+    paddingHorizontal: theme.sizes.base * 2,
+    marginBottom: theme.sizes.base * 3.5,
+  },
+ 
 });
 
 export default Browse
