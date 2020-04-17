cda2eddb475f89287f5823eecb4951f217b87753
Boa <boa.matule@gmail.com>
2020-04-18 01:01:05 +0200

    Install the slider package from react-native and imports it to the Settings.js; Defines  state variables for the slider values; Configures the de Slider component for the Budget and Monthly Cap and styles it... WIP

--- screens/Settings.js
+++ screens/Settings.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { StyleSheet, Image, ScrollView } from 'react-native';
 import { theme, mocks } from '../constants';
+import Slider from 'react-native-slider';
 
 import { Button, Block, Text, Divider } from '../components';
 export default class Browse extends React.Component {
+  state = {
+    budget: 850,
+    monthly: 1700,
+}
+
   render(){
     const { profile } = this.props;
     return (
@@ -11,10 +17,10 @@ export default class Browse extends React.Component {
          <Block flex={false} row center space="between" style={styles.header}>
             <Text h1 bold>Settings</Text>
             <Button>
-              <Image source={profile.avatar} style={styles.avatar}/>
-            </Button> 
-         </Block>
-         <ScrollView showsVerticalScrollIndicator={false}>
+            <Image source={profile.avatar} style={styles.avatar}/>
+          </Button> 
+        </Block>
+        <ScrollView showsVerticalScrollIndicator={false}>
             <Block style={styles.inputs}>
               <Block row space="between" margin={[10, 0]} style={styles.inputRow}>
                 <Block>
@@ -45,17 +51,39 @@ export default class Browse extends React.Component {
               </Block>
           </Block>
           <Divider margin={[theme.sizes.base, theme.sizes.base * 2]} />
-            <Block style={styles.sliders}>
-                <Block>
-                    <Text gray2>Budget</Text>
-                    <Text caption gray2 right>$1,000</Text>
-                </Block>
-                <Block>
-                    <Text gray2>Monthly Cap</Text>
-                    <Text caption gray2 right>$5,000</Text>
-                </Block>
-            </Block>
-            <Divider />
+            <Block style={styles.sliders}> 
+              <Block>
+                <Text gray2>Budget</Text>
+                <Slider
+                  minimumValue={0}
+                  maximumValue={1000}
+                  style={{ height: 19 }}
+                  thumbStyle={styles.thumb}
+                  trackStyle={{ height: 6, borderRadius: 6 }}
+                  minimumTrackTintColor={theme.colors.secondary}
+                  maximumTrackTintColor="rgba(157, 163, 180, 0.10)"
+                  value={this.state.budget}
+                  onValueChange={value => this.setState({ budget: value })}
+                />
+                <Text caption gray2 right>$1,000</Text>
+              </Block>
+              <Block margin={[10, 0]}>
+         <Text gray2 style={{ marginBottom: 10 }}>Monthly Cap</Text>
+         <Slider
+            minimumValue={0}
+            maximumValue={5000}
+            style={{ height: 19 }}
+            thumbStyle={styles.thumb}
+            trackStyle={{ height: 6, borderRadius: 6 }}
+            minimumTrackTintColor={theme.colors.secondary}
+            maximumTrackTintColor="rgba(157, 163, 180, 0.10)"
+            value={this.state.monthly}
+            onValueChange={value => this.setState({ monthly: value })}
+          />
+         <Text caption gray right>$5,000</Text>
+          </Block>
+          </Block>
+          <Divider />
         </ScrollView>
    </Block>
   );
@@ -84,5 +112,13 @@ const styles = StyleSheet.create({
   sliders: {
     marginTop: theme.sizes.base * 0.7,
     paddingHorizontal: theme.sizes.base * 2,
+  },
+  thumb: {
+    width: theme.sizes.base,
+    height: theme.sizes.base,
+    borderRadius: theme.sizes.base,
+    borderColor: 'white',
+    borderWidth: 3,
+    backgroundColor: theme.colors.secondary,
   },
 });
