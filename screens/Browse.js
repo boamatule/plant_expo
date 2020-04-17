3b1a8c65a8d081509eed806fd3f3ca0489e6064e
Boa <boa.matule@gmail.com>
2020-04-17 22:07:48 +0200

    Implements Tabs Section... WIP

--- screens/Browse.js
+++ screens/Browse.js
@@ -1,19 +1,46 @@
 import React from 'react';
-import { StyleSheet, Image } from 'react-native';
+import { StyleSheet, Image, TouchableOpacity } from 'react-native';
 import { theme, mocks } from '../constants';
 import { Button, Text, Block } from '../components';
 
 class Browse extends React.Component {  
+  state = {
+    active: 'Products',
+  }
+
+
+  renderTab(tab) {
+    const { active } = this.state;
+    const isActive = active === tab;
+    return(
+      <TouchableOpacity
+        key={`tab-${tab}`}
+        style={[
+        styles.tab,
+        isActive ? styles.active : null
+        ]}
+        >
+        <Text size={16} medium gray={!isActive} secondary={isActive}>
+        {tab}
+        </Text>
+      </TouchableOpacity>
+    )
+  }
   render() {
     const { profile } = this.props
+    const tabs = ['Products', 'Inspirations', 'Shop']
     return (
-     <Block flex={false} row center space="between" style={styles.header}>
-    
-        <Text h1 bold>Browse</Text>
-        <Button>
-          <Image source={profile.avatar} style={styles.avatar} />
-        </Button>
-     </Block>
+      <Block> 
+        <Block flex={false} row center space="between" style={styles.header}>    
+          <Text h1 bold>Browse</Text>
+          <Button>
+            <Image source={profile.avatar} style={styles.avatar} />
+          </Button>
+        </Block>
+        <Block flex={false} row style={styles.tabs}>
+          {tabs.map(tab => this.renderTab(tab))}
+        </Block>
+    </Block>
     );
   }
 }
@@ -30,6 +57,20 @@ const styles = StyleSheet.create({
     height: theme.sizes.base * 2.2,
     width: theme.sizes.base * 2.2,
     backgroundColor: theme.colors.grey,
+  },
+  tabs: {
+    borderBottomColor: theme.colors.gray2,
+    borderBottomWidth: 0.5,                    //for iOS : StyleSheet.hairLineWidth
+    marginVertical: theme.sizes.base,
+    marginHorizontal: theme.sizes.base * 2,
+  },
+  tab: {
+    marginRight: theme.sizes.base * 2,
+    paddingBottom: theme.sizes.base
+  },
+  active: {
+    borderBottomColor: theme.colors.secondary,
+    borderBottomWidth: 3,
   },
 });
 
