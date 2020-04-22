94f3f4027aee629a553c678c65497b4d5168eeba
Boa <boa.matule@gmail.com>
2020-04-22 11:22:08 +0200

    Defines defaultProps data from the mocks as product and defines in the render() and styles it. WIP

--- screens/Product.js
+++ screens/Product.js
@@ -1,15 +1,32 @@
 import React from 'react';
-import { Block, Text} from '../components';
+import { StyleSheet, Image, TouchableOpacity, ScrollView, Dimensions } from 'react-native';
+import { theme, mocks } from '../constants';
+import { Button, Text, Block, Card, Badge } from '../components';
+// import { styles } from '../components/Divider';
+;
 
 class Product extends React.Component {
   render() {
+    const { product } = this.props;
     return (
-      <Block middle>>
-        <Text>Browse</Text>
-      </Block>
+      <ScrollView showsVerticalScrollIndicator={false}>  
+        <Block style={styles.product}>
+          <Text h2 bold>{product.name}</Text>
+          <Text gray light height={22}>{product.description}</Text>
+        </Block>
+      </ScrollView>
     )
   }
 }
 
+Product.defaultProps = {
+  product: mocks.products[0],
+}
 
+const styles = StyleSheet.create({
+  product: {
+    paddingHorizontal: theme.sizes.base * 2,
+    paddingVertical: theme.sizes.padding,
+  }
+})
 export default Product
